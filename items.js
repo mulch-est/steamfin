6844c4b38223e61b4a3dbd66750be6e956315f90
mulch <52766118+mulch-est@users.noreply.github.com>
2022-08-06 21:17:34 -0400

    Create items.js

---
+++ items.js
@@ -0,0 +1,9 @@
+/* this file will contain the items that can be used with steam's multisell system,
+including their different potential spellings, etc. and categorized by collection */
+
+/*An array containing (hopefully) all multi-sellable CSGO items:*/
+var items = [];
+
+for(let i=0; i<cases.length; i++){
+  items.push(cases[i]);
+}
