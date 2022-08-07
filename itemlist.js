ed0561c163c680606ef370df37ca45f24475bc63
mulch <52766118+mulch-est@users.noreply.github.com>
2022-08-07 14:33:19 -0400

    item list now contains remove buttons (unusable)

--- itemlist.js
+++ itemlist.js
@@ -9,10 +9,22 @@ function attemptPush(){
     //push valid item to list
     list.push(data);
     //add div to page
-    document.getElementById("itemsDiv").innerHTML += "<p>"+data+"</p>"
+    var itemList = document.getElementById("itemsDiv");
+    let a = document.createElement("DIV");
+    a.setAttribute("class", "item-bar");
+    a.innerHTML = "<a>"+data+"</a>";
+    //onclick="unlist("+data+")"
+    a.innerHTML += "<input type='button' onclick='' value='x' />";
+    itemList.parentNode.appendChild(a);
+    
+    //itemList.innerHTML += "<p>"+data+"</p>"
     //remove text from search
     document.getElementById("searchAuto").value = "";
   }
+}
+
+function unlist(item){
+  //stub
 }
 
 //checks whether an element in items (items.js) is equal to <data> and not in list
