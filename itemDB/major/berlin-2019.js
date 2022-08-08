c106e5a1346e9e61a79d7323f92854b1a53d5987
mulch <52766118+mulch-est@users.noreply.github.com>
2022-08-08 15:32:21 -0400

    Create berlin-2019.js

---
+++ itemDB/major/berlin-2019.js
@@ -0,0 +1,6 @@
+berlin19 = ["Berlin 2019 Returning Challengers (Holo/Foil)",
+            "Berlin 2019 Minor Challengers (Holo/Foil)",
+            "Berlin 2019 Legends (Holo/Foil)",
+            "Berlin 2019 Returning Challengers Autograph Capsule",
+            "Berlin 2019 Minor Challengers Autograph Capsule",
+            "Berlin 2019 Legends Autograph Capsule"];
