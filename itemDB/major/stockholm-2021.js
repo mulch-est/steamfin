0abb5c28c6e6eeb7bc61707b863c373aca3c2521
mulch <52766118+mulch-est@users.noreply.github.com>
2022-08-08 15:31:42 -0400

    Create stockholm-2021.js

---
+++ itemDB/major/stockholm-2021.js
@@ -0,0 +1,5 @@
+var stockholm21 = ["Stockholm 2021 Challengers Sticker Capsule",
+                   "Stockholm 2021 Contenders Sticker Capsule",
+                   "Stockholm 2021 Legends Sticker Capsule",
+                   "Stockholm 2021 Champions Autograph Capsule",
+                   "Stockholm 2021 Finalists Autograph Capsule"];
