a02af7c3828af07d6299be1b8fe9c57fd64fac25
mulch <52766118+mulch-est@users.noreply.github.com>
2022-08-10 13:03:41 -0400

    added support for passes and 2021 community capsule

--- items.js
+++ items.js
@@ -1,26 +1,30 @@
 /* this file will contain the items that can be used with steam's multisell system,
 including their different potential spellings, etc. and categorized by collection */
 
-/*An array containing (hopefully) all multi-sellable CSGO items:*/
+/*A 2d array containing (hopefully) all multi-sellable CSGO items:*/
 var items = [];
+/*A 2d array containing (hopefully) all CSGO sticker collection arrays:*/
+var sticker_collections = [];
 
 items.push(cases); //add all items from cases.js
 items.push(capsules);
-items.push(antwerp22); //add all items from antwerp-2022.js
+items.push(passes);
+
+/*items.push(antwerp22);
 items.push(comm21);
 items.push(stockholm21);
 items.push(rmr20);
-items.push(berlin19);
+items.push(berlin19);*/
 
-items.push(OPbrfa);
-items.push(recoil);
+pushStickerCollectionToItems(OPbrfa);
+pushStickerCollectionToItems(recoil);
+pushStickerCollectionToItems(comm21);
 
 //use itemDB arrays to filter
 
-function pushToItems(arr){
-  for(let i=0; i<arr.length; i++){
-    items.push(arr[i]);
-  }
+function pushStickerCollectionToItems(arr){
+  items.push(arr);
+  sticker_collections.push(arr);
 }
 
 /* called from autocomplete to check if int value represents sticker collection arr */
@@ -29,11 +33,7 @@ function isValidArrVal(arr, val){
   return false;
 }
 
-var sticker_collections = [];
-
-sticker_collections.push(OPbrfa);
-sticker_collections.push(recoil);
-
+/* called on page run to dynamically add all sticker collections from itemDB */
 function generateStickerCollectionsDropdown(){
   var d = document.getElementById('stickerCollectionsDropdown');
   for(let i = 1; i <= sticker_collections.length; i++){
@@ -45,12 +45,15 @@ function generateStickerCollectionsDropdown(){
 }
 
 function getCollectionTitle(arr){
-  if(arr === OPbrfa){
-    return "Broken Fang Sticker Collection";
-  }else if(arr === recoil){
-    return "Recoil Sticker Collection";
-  }else {
-    return "ERROR!";
+  switch(arr) {
+    case OPbrfa:
+      return "Broken Fang Sticker Collection";
+    case recoil:
+      return "Recoil Sticker Collection";
+    case comm21:
+      return "2021 Community Sticker Capsule";
+    default:
+      return "ERROR!";
   }
 }
 
