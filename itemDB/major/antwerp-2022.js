0057a403c7e2263504e17067d15e87c016943a02
mulch <52766118+mulch-est@users.noreply.github.com>
2022-08-10 12:58:30 -0400

    remove capsules

--- itemDB/major/antwerp-2022.js
+++ itemDB/major/antwerp-2022.js
@@ -1,3 +1,1 @@
-var antwerp22 = ["Antwerp 2022 Challengers Sticker Capsule",
-                "Antwerp 2022 Contenders Sticker Capsule",
-                "Antwerp 2022 Legends Sticker Capsule"];
+var antwerp22 = [];
