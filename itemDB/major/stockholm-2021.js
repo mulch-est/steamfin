8574605f4f52dbd99434477f6e80c70e72bc2965
mulch <52766118+mulch-est@users.noreply.github.com>
2022-08-10 12:59:13 -0400

    remove capsules

--- itemDB/major/stockholm-2021.js
+++ itemDB/major/stockholm-2021.js
@@ -1,5 +1,1 @@
-var stockholm21 = ["Stockholm 2021 Challengers Sticker Capsule",
-                   "Stockholm 2021 Contenders Sticker Capsule",
-                   "Stockholm 2021 Legends Sticker Capsule",
-                   "Stockholm 2021 Champions Autograph Capsule",
-                   "Stockholm 2021 Finalists Autograph Capsule"];
+var stockholm21 = [];
